12c50a2757b9cc1a8aafa976a07c97d73ce81843
Stephen Hodgson <hodgson.designs@gmail.com>
2024-04-15 09:23:10 -0400

    com.utilities.websockets 1.0.0-preview.1

---
+++ Utilities.Websockets/Packages/com.utilities.websockets/Tests/TestFixture_01.cs
@@ -0,0 +1,16 @@
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Utilities.Websockets.Tests
+{
+    internal class TestFixture_01
+    {
+        [Test]
+        public async Task Test_01()
+        {
+            await Task.CompletedTask;
+        }
+    }
+}
