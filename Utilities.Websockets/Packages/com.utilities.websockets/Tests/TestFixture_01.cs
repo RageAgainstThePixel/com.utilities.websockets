78036f69325616e36f530daca6afcfba5edd4abe
Stephen Hodgson <rage.against.the.pixel@gmail.com>
2025-11-01 21:52:06 -0400

    update tests

--- Utilities.Websockets/Packages/com.utilities.websockets/Tests/TestFixture_01.cs
+++ Utilities.Websockets/Packages/com.utilities.websockets/Tests/TestFixture_01.cs
@@ -3,6 +3,8 @@
 using System;
 using System.Threading.Tasks;
 using NUnit.Framework;
+using UnityEngine;
+using Utilities.Async;
 
 namespace Utilities.WebSockets.Tests
 {
@@ -13,47 +15,55 @@ namespace Utilities.WebSockets.Tests
         [Test]
         public async Task Test_01_SimpleEcho()
         {
-            var openTcs = new TaskCompletionSource<bool>();
-            var messageTcs = new TaskCompletionSource<bool>();
-            var closeTcs = new TaskCompletionSource<bool>();
-            using var socket = new WebSocket(EchoServerAddress);
-            socket.OnOpen += Socket_OnOpen;
-            socket.OnMessage += Socket_OnMessage;
-            socket.OnError += Socket_OnError;
-            socket.OnClose += Socket_OnClose;
-            socket.Connect();
-
-            var result = await openTcs.Task;
-            Assert.IsTrue(result);
-            await socket.SendAsync("hello world!");
-            result = await messageTcs.Task;
-            Assert.IsTrue(result);
-            await socket.CloseAsync();
-            result = await closeTcs.Task;
-            Assert.IsTrue(result);
-
-            void Socket_OnOpen()
+            try
             {
-                UnityEngine.Debug.Log("Socket_OnOpen");
-                openTcs.TrySetResult(true);
+                await Awaiters.UnityMainThread;
+                var openTcs = new TaskCompletionSource<bool>();
+                var messageTcs = new TaskCompletionSource<bool>();
+                var closeTcs = new TaskCompletionSource<bool>();
+                using var socket = new WebSocket(EchoServerAddress);
+                socket.OnOpen += Socket_OnOpen;
+                socket.OnMessage += Socket_OnMessage;
+                socket.OnError += Socket_OnError;
+                socket.OnClose += Socket_OnClose;
+                socket.Connect();
+
+                var result = await openTcs.Task;
+                Assert.IsTrue(result);
+                await socket.SendAsync("hello world!");
+                result = await messageTcs.Task;
+                Assert.IsTrue(result);
+                await socket.CloseAsync();
+                result = await closeTcs.Task;
+                Assert.IsTrue(result);
+
+                void Socket_OnOpen()
+                {
+                    UnityEngine.Debug.Log("Socket_OnOpen");
+                    openTcs.TrySetResult(true);
+                }
+
+                void Socket_OnMessage(DataFrame dataFrame)
+                {
+                    UnityEngine.Debug.Log($"Socket_OnMessage: {dataFrame.Text}");
+                    messageTcs.TrySetResult(true);
+                }
+
+                void Socket_OnError(Exception exception)
+                {
+                    UnityEngine.Debug.LogException(exception);
+                    Assert.Fail(exception.Message);
+                }
+
+                void Socket_OnClose(CloseStatusCode arg1, string arg2)
+                {
+                    UnityEngine.Debug.Log("Socket_OnClose");
+                    closeTcs.TrySetResult(true);
+                }
             }
-
-            void Socket_OnMessage(DataFrame dataFrame)
+            catch (Exception e)
             {
-                UnityEngine.Debug.Log($"Socket_OnMessage: {dataFrame.Text}");
-                messageTcs.TrySetResult(true);
-            }
-
-            void Socket_OnError(Exception exception)
-            {
-                UnityEngine.Debug.LogException(exception);
-                Assert.Fail(exception.Message);
-            }
-
-            void Socket_OnClose(CloseStatusCode arg1, string arg2)
-            {
-                UnityEngine.Debug.Log("Socket_OnClose");
-                closeTcs.TrySetResult(true);
+                Debug.LogException(e);
             }
         }
     }
