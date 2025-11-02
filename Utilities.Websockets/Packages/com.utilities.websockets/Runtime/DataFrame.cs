4b8cb32d85a669352b3a549cea38e461e934b712
Stephen Hodgson <rage.against.the.pixel@gmail.com>
2025-11-02 01:30:09 -0500

    com.utilities.websockets 2.0.0 (#21)
    - refactor message queue to use more modern implementation
    - com.utilities.async -> 3.0.2
    - com.utilities.extensions -> 1.3.6

--- Utilities.Websockets/Packages/com.utilities.websockets/Runtime/DataFrame.cs
+++ Utilities.Websockets/Packages/com.utilities.websockets/Runtime/DataFrame.cs
@@ -1,24 +1,29 @@
 // Licensed under the MIT License. See LICENSE in the project root for license information.
 
 using System;
+using Unity.Collections;
+using Utilities.Extensions;
 
 namespace Utilities.WebSockets
 {
-    public class DataFrame
+    public readonly struct DataFrame : IDisposable
     {
         public OpCode Type { get; }
 
-        public ReadOnlyMemory<byte> Data { get; }
+        public NativeArray<byte> Data { get; }
 
-        public string Text { get; }
+        public string Text
+            => Type == OpCode.Text
+                ? System.Text.Encoding.UTF8.GetString(Data.AsSpan())
+                : string.Empty;
 
-        public DataFrame(OpCode type, ReadOnlyMemory<byte> data)
+        public DataFrame(OpCode type, NativeArray<byte> data)
         {
             Type = type;
             Data = data;
-            Text = type == OpCode.Text
-                ? System.Text.Encoding.UTF8.GetString(data.Span)
-                : string.Empty;
         }
+
+        public void Dispose()
+            => Data.Dispose();
     }
 }
--- Utilities.Websockets/Packages/com.utilities.websockets/Tests/TestFixture_01.cs
+++ Utilities.Websockets/Packages/com.utilities.websockets/Tests/TestFixture_01.cs
@@ -1,8 +1,10 @@
 // Licensed under the MIT License. See LICENSE in the project root for license information.
 
+using NUnit.Framework;
 using System;
 using System.Threading.Tasks;
-using NUnit.Framework;
+using Unity.Collections;
+using UnityEngine;
 
 namespace Utilities.WebSockets.Tests
 {
@@ -10,50 +12,74 @@ namespace Utilities.WebSockets.Tests
     {
         private const string EchoServerAddress = "wss://echo.websocket.org";
 
+        private NativeLeakDetectionMode originalLeakDetectionMode;
+
+        [SetUp]
+        public void Setup()
+        {
+            originalLeakDetectionMode = NativeLeakDetection.Mode;
+            NativeLeakDetection.Mode = NativeLeakDetectionMode.EnabledWithStackTrace;
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            NativeLeakDetection.Mode = originalLeakDetectionMode;
+        }
+
         [Test]
         public async Task Test_01_SimpleEcho()
         {
-            var openTcs = new TaskCompletionSource<bool>();
-            var messageTcs = new TaskCompletionSource<bool>();
-            var closeTcs = new TaskCompletionSource<bool>();
-            using var socket = new WebSocket(EchoServerAddress);
-            socket.OnOpen += Socket_OnOpen;
-            socket.OnMessage += Socket_OnMessage;
-            socket.OnError += Socket_OnError;
-            socket.OnClose += Socket_OnClose;
-            socket.Connect();
-
-            var result = await openTcs.Task;
-            Assert.IsTrue(result);
-            await socket.SendAsync("hello world!");
-            result = await messageTcs.Task;
-            Assert.IsTrue(result);
-            await socket.CloseAsync();
-            result = await closeTcs.Task;
-            Assert.IsTrue(result);
-
-            void Socket_OnOpen()
+            try
             {
-                UnityEngine.Debug.Log("Socket_OnOpen");
-                openTcs.TrySetResult(true);
+                var temp = new NativeArray<byte>(0, Allocator.Temp);
+                Assert.IsTrue(temp.IsCreated);
+                var openTcs = new TaskCompletionSource<bool>();
+                var messageTcs = new TaskCompletionSource<bool>();
+                var closeTcs = new TaskCompletionSource<bool>();
+                using var socket = new WebSocket(EchoServerAddress);
+                socket.OnOpen += Socket_OnOpen;
+                socket.OnMessage += Socket_OnMessage;
+                socket.OnError += Socket_OnError;
+                socket.OnClose += Socket_OnClose;
+                socket.Connect();
+
+                var result = await openTcs.Task;
+                Assert.IsTrue(result);
+                await socket.SendAsync("hello world!");
+                result = await messageTcs.Task;
+                Assert.IsTrue(result);
+                await socket.CloseAsync();
+                result = await closeTcs.Task;
+                Assert.IsTrue(result);
+
+                void Socket_OnOpen()
+                {
+                    Debug.Log("Socket_OnOpen");
+                    openTcs.TrySetResult(true);
+                }
+
+                void Socket_OnMessage(DataFrame dataFrame)
+                {
+                    Debug.Log($"Socket_OnMessage: {dataFrame.Text}");
+                    messageTcs.TrySetResult(true);
+                }
+
+                void Socket_OnError(Exception exception)
+                {
+                    Debug.LogException(exception);
+                    Assert.Fail(exception.Message);
+                }
+
+                void Socket_OnClose(CloseStatusCode arg1, string arg2)
+                {
+                    Debug.Log("Socket_OnClose");
+                    closeTcs.TrySetResult(true);
+                }
             }
-
-            void Socket_OnMessage(DataFrame dataFrame)
+            catch (Exception e)
             {
-                UnityEngine.Debug.Log($"Socket_OnMessage: {dataFrame.Text}");
-                messageTcs.TrySetResult(true);
-            }
-
-            void Socket_OnError(Exception exception)
-            {
-                UnityEngine.Debug.LogException(exception);
-                Assert.Fail(exception.Message);
-            }
-
-            void Socket_OnClose(CloseStatusCode arg1, string arg2)
-            {
-                UnityEngine.Debug.Log("Socket_OnClose");
-                closeTcs.TrySetResult(true);
+                Debug.LogException(e);
             }
         }
     }
